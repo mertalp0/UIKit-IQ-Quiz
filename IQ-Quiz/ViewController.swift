5e17c665babd1c9654ee163fbc68195cbfede011
mertalp <mertalp010@gmail.com>
2024-08-19 17:39:52 +0300

    created project

---
+++ IQ-Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IQ-Quiz
+//
+//  Created by mert alp on 19.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
